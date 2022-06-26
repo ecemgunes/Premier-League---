fcdd462c173d9eba2a72a141b74c06c61fd2408c
ecemgunes <75450296+ecemgunes@users.noreply.github.com>
2022-06-26 20:35:18 +0300

    Update index.php

--- proje/index.php
+++ proje/index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Şampiyonlar Ligi</title>
+    <title>Premier League</title>
     <link rel="stylesheet" type="text/css" media="all" href="style.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </head>
